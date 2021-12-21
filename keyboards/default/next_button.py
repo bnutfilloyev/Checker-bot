7e5dda9741e7886557150883bbf94d6bec6089ba
thanhson <steven.n.t@outlook.com>
2021-12-21 16:48:39 +0700

    Personalized.

--- keyboards/default/next_button.py
+++ keyboards/default/next_button.py
@@ -3,7 +3,7 @@ from aiogram.types.reply_keyboard import ReplyKeyboardMarkup, KeyboardButton
 next = ReplyKeyboardMarkup(
     keyboard=[
         [
-            KeyboardButton(text='⏩next')
+            KeyboardButton(text='⏩NEXT')
         ]
     ],
     one_time_keyboard=True,
--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -8,7 +8,7 @@ from data.config import ADMINS
 async def on_startup_notify(dp: Dispatcher):
     for admin in ADMINS:
         try:
-            await dp.bot.send_message(admin, "Alhamdulillah🌙")
+            await dp.bot.send_message(admin, "The referral bot has been launched")
 
         except Exception as err:
             logging.exception(err)
